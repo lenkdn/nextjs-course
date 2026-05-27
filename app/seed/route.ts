e4c5197f370b2997843629bddabb2c4a14599a27
Daniil <lenkdn01@gmail.com>
2026-05-27 13:47:20 +0300

    chapter 6 completed

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,11 +1,17 @@
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
-import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import {
+  customers,
+  invoices,
+  revenue,
+  users,
+} from "@/app/lib/placeholder-data";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import bcrypt from "bcrypt";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!);
 
 async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
   await sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -30,7 +36,7 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
 
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
@@ -56,7 +62,7 @@ async function seedInvoices() {
 }
 
 async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`;
 
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
@@ -110,7 +116,7 @@ export async function GET() {
       seedRevenue(),
     ]);
 
-    return Response.json({ message: 'Database seeded successfully' });
+    return Response.json({ message: "Database seeded successfully" });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }

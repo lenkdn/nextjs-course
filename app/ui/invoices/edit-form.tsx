50320125f7447e9b7e3b62e01f78046613df048f
Daniil <lenkdn01@gmail.com>
2026-05-28 18:16:16 +0300

    chapter 13 completed

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -7,10 +7,11 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
+import { State, updateInvoice } from "@/app/lib/actions";
 
 import { Button } from "@/app/ui/button";
 import Link from "next/link";
-import { updateInvoice } from "@/app/lib/actions";
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -21,8 +22,15 @@ export default function EditInvoiceForm({
 }) {
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
+  const initialState: State = { message: null, errors: {} };
+
+  const [state, submitAction] = useActionState(
+    updateInvoiceWithId,
+    initialState,
+  );
+
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={submitAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -63,10 +71,19 @@ export default function EditInvoiceForm({
                 step="0.01"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
+                aria-describedby="amount-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -112,6 +129,12 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+
+        <div>
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
